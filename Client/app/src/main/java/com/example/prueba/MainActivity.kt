ab810ab21a0f54b929380d8cdae610684068c215
Juan Manuel Sabanes <jmsabanes07@gmail.com>
2023-08-17 22:01:31 -0300

    agregue un boton y una nueva pantalla para testear el funcionamiento del enrutador

--- Client/app/src/main/java/com/example/prueba/MainActivity.kt
+++ Client/app/src/main/java/com/example/prueba/MainActivity.kt
@@ -12,14 +12,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnRandomize: Button = findViewById(R.id.randomize)
-        val textValue: TextView = findViewById(R.id.textValue)
+        val btnChangePage: Button = findViewById(R.id.changePage)
+        val textValue: TextView = findViewById(R.id.Text)
 
         btnRandomize.setOnClickListener{
-            textValue.text = rand(50).toString()
+            val number = rand(50).toString()
+            println(number)
+            textValue.text = number
         }
+        btnChangePage.setOnClickListener{
+            goTo("layout2")
+        }
+
     }
 
     private fun rand(max: Int): Int{
         return (0..max).random()
+    }
+    private fun goTo(page: String) {
+        val layoutId = resources.getIdentifier(page, "layout", packageName)
+
+        println(layoutId)
+
+        if (layoutId == 0) {
+            return Toast.makeText(this, "No se encontró diseño para la página: $page", Toast.LENGTH_LONG).show()
+        }
+
+        setContentView(layoutId)
     }
 }
